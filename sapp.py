d60558f136089efdcf05ae0898105b58e33c0efb
erkamersaherging <102802657+erkamersaherging@users.noreply.github.com>
2022-07-27 01:31:45 +0200

    Update sapp.py

--- sapp.py
+++ sapp.py
@@ -1,19 +1,10 @@
-from tokenize import PlainToken
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import numpy as np
 from recommender import recommend_with_NMF,cluster_recomender
 
 df = pd.read_csv('data/movies_clusters.csv')
 
 st.header('RECOMMENDER v.3000')
-
-a = ['Quest for Camelot', 'Erik the Viking', "Twelve Tasks of Asterix, The (Les douze travaux d'Astérix)", 'Toy Story 2', 'Space Buddies']
-
-st.write(cluster_recomender(a[0])[0])
 
 
 col1, col2 = st.columns([3, 1])
@@ -96,73 +87,4 @@ if col33.button('show similar'):
         # cols2[i].write(cluster_recomender(rec_list[0])[0])
         for j in range(5):
             cols2[i].write(cluster_recomender(rec_list[i])[j])
-
-
-a = ['Quest for Camelot', 'Erik the Viking', "Twelve Tasks of Asterix, The (Les douze travaux d'Astérix)", 'Toy Story 2', 'Space Buddies']
-
-st.write(cluster_recomender(a[0])[0])
-
-
-
-
-# if st.button('reset'):
-#     st.experimental_rerun()
-
-
-
-
-    
-
-
-
-
-# title = st.text_input('Movie title', 'Life of Brian')
-# st.write('The current movie title is', title)
-
-
-
-
-#user_input = st.text_area("label goes here", default_value_goes_here)
-
-# user_movie_1 = st.selectbox(
-#      'Which movie you would like to rate?',
-#      df['title'])
-
-# user_rate_1 = st.selectbox(
-#      f'Rate {user_movie_1}',
-#      (1,2,3,4,5))
-
-
-
-# st.write(f'You rated {user_movie_1}, {user_rate_1}')
-
-
-# Add a selectbox to the sidebar:
-# add_selectbox = st.sidebar.selectbox(
-#     'How would you like to be contacted?',
-#     ('Email', 'Home phone', 'Mobile phone')
-# )
-
-# # Add a slider to the sidebar:
-# add_slider = st.sidebar.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0)
-# )
-
-
-# from PIL import Image
-# image = Image.open('sunrise.jpg')
-
-# st.image(image, caption='Sunrise by the mountains')
-
-# page_bg_img = '''
-# <style>
-# body {
-# background-image: url("https://images.unsplash.com/photo-1490059830487-2f86fddb2b4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80");
-# background-size: cover;
-# }
-# </style>
-# '''
-
-# st.markdown(page_bg_img, unsafe_allow_html=True)
 
